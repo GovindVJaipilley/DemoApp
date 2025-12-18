30a2c2f9d68456fc19c0624098e6dc5535756282
GovindVJaipilley <GovindJaipilley045@gmail.com>
2025-12-18 11:48:42 +0530

    multipleLocators.spec.js

--- PlayWrightAutomation/tests/multipleLocators.spec.js
+++ PlayWrightAutomation/tests/multipleLocators.spec.js
@@ -12,7 +12,7 @@ test('Multiple Locators Practice', async ({page}) => {
         const linkText = await link.textContent();
         console.log('Link Text: ', linkText);
     }
-    await page.waitForSelector("//div[@id='tbodyid']//div/h4/a");
+  /*  await page.waitForSelector("//div[@id='tbodyid']//div/h4/a");
     const productsList = await page.locator("//div[@id='tbodyid']//div/h4/a");
     const productsCount = await productsList.count();
     console.log('Total number of products: ', productsCount);
@@ -21,6 +21,14 @@ test('Multiple Locators Practice', async ({page}) => {
     for(let i=0; i<productsCount; i++){
         const productName = await productsList.nth(i).textContent();
         console.log('Product Name: ', productName);
+    }*/
+
+    await page.waitForSelector("//div[@id='tbodyid']//div/h4/a");
+    const productsList = await page.$$("//div[@id='tbodyid']//div/h4/a");
+    console.log('Number of Products is: ', (await productsList).length);
+    for(const product of productsList){
+        const productName = await product.textContent();
+        console.log('Product Name is: ', productName);
     }
 
     await page.close();
